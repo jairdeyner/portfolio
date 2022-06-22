2fdc709f65ccfaf3f33be06435555a3a22562936
jair <jairdeyner730791@gmail.com>
2022-06-22 00:17:16 -0500

    refactor: chip text changed

--- src/components/atoms/Chip.tsx
+++ src/components/atoms/Chip.tsx
@@ -3,7 +3,7 @@ import styles from "./Chip.module.scss";
 const Chip = () => {
   return (
     <div className={styles.chip}>
-      <p className={styles.chip__text}>Desarollador Web</p>
+      <p className={styles.chip__text}>Desarrollador Web</p>
     </div>
   );
 };
