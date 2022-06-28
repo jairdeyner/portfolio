e0ab9c21c86f3534a2000307a990b73fc67fa55d
jair <jairdeyner730791@gmail.com>
2022-06-28 16:40:43 -0500

    chore: skill icons added

---
+++ src/assets/images/skills/index.ts
@@ -0,0 +1,23 @@
+import skillCss from "./skill_css.svg";
+import skillFigma from "./skill_figma.svg";
+import skillFirebase from "./skill_firebase.svg";
+import skillGit from "./skill_git.svg";
+import skillGithub from "./skill_github.svg";
+import skillHtml from "./skill_html.svg";
+import skillJava from "./skill_java.svg";
+import skillJs from "./skill_js.svg";
+import skillMysql from "./skill_mysql.svg";
+import skillReact from "./skill_react.svg";
+
+export {
+  skillCss,
+  skillFigma,
+  skillFirebase,
+  skillGit,
+  skillGithub,
+  skillHtml,
+  skillJava,
+  skillJs,
+  skillMysql,
+  skillReact,
+};
