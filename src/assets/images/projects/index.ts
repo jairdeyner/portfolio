a2774b1b760ff2df711359e6442d70d3b8c41440
jair <jairdeyner730791@gmail.com>
2022-06-29 00:07:19 -0500

    chore: added images

---
+++ src/assets/images/projects/index.ts
@@ -0,0 +1,5 @@
+import calvicScreen from "./calvic-screen.png";
+import gifexpertScreen from "./gifexpert-screen.png";
+import portfolioScreen from "./portfolio-screen.png";
+
+export { calvicScreen, gifexpertScreen, portfolioScreen };
