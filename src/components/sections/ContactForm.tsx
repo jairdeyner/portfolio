ef97e42e08fc3f66e129589c93ff50e7eb0d8085
jair <jairdeyner730791@gmail.com>
2022-07-01 19:04:29 -0500

    feat: config formik components

--- src/components/sections/ContactForm.tsx
+++ src/components/sections/ContactForm.tsx
@@ -1,76 +1,111 @@
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 import Button from "../atoms/Button";
 
 import styles from "./ContactForm.module.scss";
 
 const ContactForm = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Mensaje enviandose");
-  };
-
   return (
     <section className={styles.contactForm}>
-      <form
-        className={styles.contactForm__form}
-        onSubmit={handleSubmit}
-        noValidate
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          message: "",
+        }}
+        onSubmit={(values, { resetForm }) => {
+          console.log(values);
+          resetForm();
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string().required("Por favor ingrese un nombre"),
+          email: Yup.string()
+            .email("Por favor ingrese un correo válido")
+            .required("Por favor ingrese un correo"),
+          message: Yup.string().required("Por favor ingrese un mensaje"),
+        })}
       >
-        <div className={styles.contactForm__inputBox}>
-          <div className={styles.contactForm__formControlContainer}>
-            <label className={styles.contactForm__label} htmlFor="name">
-              Nombre:
-            </label>
-            <input
-              className={styles.contactForm__formControl}
-              type="text"
-              autoComplete="off"
-              id="name"
-              placeholder="Jair Tello"
-            />
-            <span className={styles.contactForm__text}>
-              El nombre es obligaorio
-            </span>
-          </div>
-          <div className={styles.contactForm__formControlContainer}>
-            <label className={styles.contactForm__label} htmlFor="email">
-              Correo:
-            </label>
-            <input
-              className={styles.contactForm__formControl}
-              type="email"
-              autoComplete="off"
-              id="email"
-              placeholder="jairdeyner730791@gmail.com"
-            />
-            {/* <span className={styles.contactForm__text}>
-              El correo es obligaorio
-            </span> */}
-          </div>
-        </div>
-        <div className={styles.contactForm__formControlContainer}>
-          <label className={styles.contactForm__label} htmlFor="message">
-            Mensaje:
-          </label>
-          <textarea
-            className={styles.contactForm__formControl}
-            placeholder="Mensaje :)"
-            id="message"
-            autoComplete="off"
-            rows={5}
-          ></textarea>
-          {/* <span className={styles.contactForm__text}>
-            El mensaje no debe estar vacío
-          </span> */}
-        </div>
-        <div className={styles.contactForm__buttonBox}>
-          <Button
-            model="ghost"
-            size="normal"
-            type="submit"
-            value="Enviar Mensaje"
-          />
-        </div>
-      </form>
+        {formik => (
+          <Form className={styles.contactForm__form}>
+            <div className={styles.contactForm__inputBox}>
+              <div className={styles.contactForm__formControlContainer}>
+                <label className={styles.contactForm__label} htmlFor="name">
+                  Nombre:
+                </label>
+                <Field
+                  name="name"
+                  type="text"
+                  autoComplete="off"
+                  className={`${styles.contactForm__formControl} ${
+                    formik.errors.name && formik.touched.name
+                      ? styles["contactForm__formControl-error"]
+                      : ""
+                  }`}
+                  id="name"
+                  placeholder="Jair Tello"
+                />
+                <ErrorMessage name="name">
+                  {msg => (
+                    <span className={styles.contactForm__text}>{msg}</span>
+                  )}
+                </ErrorMessage>
+              </div>
+              <div className={styles.contactForm__formControlContainer}>
+                <label className={styles.contactForm__label} htmlFor="email">
+                  Correo:
+                </label>
+                <Field
+                  name="email"
+                  type="email"
+                  autoComplete="off"
+                  className={`${styles.contactForm__formControl} ${
+                    formik.errors.email && formik.touched.email
+                      ? styles["contactForm__formControl-error"]
+                      : ""
+                  }`}
+                  id="email"
+                  placeholder="jairdeyner730791@gmail.com"
+                />
+                <ErrorMessage name="email">
+                  {msg => (
+                    <span className={styles.contactForm__text}>{msg}</span>
+                  )}
+                </ErrorMessage>
+              </div>
+            </div>
+            <div className={styles.contactForm__formControlContainer}>
+              <label className={styles.contactForm__label} htmlFor="message">
+                Mensaje:
+              </label>
+              <Field
+                name="message"
+                as="textarea"
+                autoComplete="off"
+                className={`${styles.contactForm__formControl} ${
+                  formik.errors.message && formik.touched.message
+                    ? styles["contactForm__formControl-error"]
+                    : ""
+                }`}
+                id="message"
+                placeholder="Mensaje :)"
+                rows={5}
+              />
+              <ErrorMessage name="message">
+                {msg => <span className={styles.contactForm__text}>{msg}</span>}
+              </ErrorMessage>
+            </div>
+            <div className={styles.contactForm__buttonBox}>
+              <Button
+                model="ghost"
+                size="normal"
+                type="submit"
+                value="Enviar Mensaje"
+              />
+            </div>
+          </Form>
+        )}
+      </Formik>
     </section>
   );
 };
