519b096ca5ec3b94d17472587c66ca0441d3740d
Jair Deyner <jairdeyner730791@gmail.com>
2022-07-25 17:05:29 -0500

    chore: modified text

--- src/components/atoms/Chip.tsx
+++ src/components/atoms/Chip.tsx
@@ -3,7 +3,7 @@ import styles from "./Chip.module.scss";
 const Chip = () => {
   return (
     <div className={styles.chip}>
-      <p className={styles.chip__text}>Desarrollador Web</p>
+      <p className={styles.chip__text}>Desarrollador Front-End</p>
     </div>
   );
 };
